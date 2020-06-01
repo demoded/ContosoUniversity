2039fd23b9b8d3929bb72500500fdc35cd460cdd
demoded <demoded@live.com>
2020-06-01 20:13:35 +1200

    EF Core with Razor pages app from docs.microsoft.com

---
+++ ContosoUniversity/Models/Enrollment.cs
@@ -0,0 +1,21 @@
+﻿
+using System.Threading;
+
+namespace ContosoUniversity.Models
+{
+    public enum Grade
+    { 
+        A,B,C,D,E,F
+    }
+
+    public class Enrollment
+    {
+        public int EnrollmentId { get; set; }
+        public int CourseId { get; set; }
+        public int StudentId { get; set; }
+        public Grade? Grade { get; set; }
+
+        public Course Course { get; set; }
+        public Student Student { get; set; }
+    }
+}
